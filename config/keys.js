3be59b8604394a2a7ac029f765259d633adc3291
tranki1 <kimhieutran100@gmail.com>
2018-07-24 09:53:44 +0300

    Connecting To MongoDB With Mongoose

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://admin:SAN748kkj@ds145921.mlab.com:45921/nerdnet"
+};
--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
+
+// DB config
+const db = require("./config/keys").mongoURI;
+
+//connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
 app.get("/", (req, res) => res.send("Hello"));
+
+//for heroku port and local port
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
