4e682e5c4ba017f2adf05fc9be1e33b593bd34f4
tranki1 <kimhieutran100@gmail.com>
2018-07-24 12:54:38 +0300

    email and password login

--- routes/api/users.js
+++ routes/api/users.js
@@ -11,7 +11,7 @@ const User = require("../../models/User");
 // @access public
 router.get("/test", (req, res) => res.json({ msg: "users works" }));
 
-// @route GET api/users/register
+// @route POST api/users/register
 // @desc Register users
 // @access Public
 router.post("/register", (req, res) => {
@@ -48,6 +48,31 @@ router.post("/register", (req, res) => {
         });
       });
     }
+  });
+});
+
+// @route POST api/users/login
+// @desc Login user / Returning JWT token
+// @access Public
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  //Find user by email
+  User.findOne({ email }).then(user => {
+    //Check for user
+    if (!user) {
+      return res.status(404).json({ email: "User not found" });
+    }
+
+    //check password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        res.json({ msg: "Sucess" });
+      } else {
+        return res.status(400).json({ password: "Password incorrect" });
+      }
+    });
   });
 });
 
