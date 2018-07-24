c2e2a37e118f267f25b8de112b80cf2d06e791d5
tranki1 <kimhieutran100@gmail.com>
2018-07-24 12:32:47 +0300

    user registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,54 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+
+// Load User model
+const User = require("../../models/User");
 
 // @route GET api/users/test
 // @desc Tests users route
 // @access public
 router.get("/test", (req, res) => res.json({ msg: "users works" }));
+
+// @route GET api/users/register
+// @desc Register users
+// @access Public
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    //if user exist
+    if (user) {
+      return res.status(400).json({ email: "Email already exists" });
+    }
+    //else create new user
+    else {
+      //using gravatar libary for node
+      const avatar = gravatar.url(req.body.email, {
+        s: "200", //size of the avatar
+        r: "pg", //rating of the avatar
+        d: "mm" //default
+      });
+
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar,
+        password: req.body.password
+      });
+
+      //hash the password
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,8 +4,13 @@ const mongoose = require("mongoose");
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+//body pareser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // DB config
 const db = require("./config/keys").mongoURI;
