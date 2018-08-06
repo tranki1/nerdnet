c054674bdb3232a99502b94f8cef7660f184f40b
tranki1 <kimhieutran100@gmail.com>
2018-08-06 11:15:07 +0300

    Post State & addPost Action

---
+++ client/src/actions/postActions.js
@@ -0,0 +1,19 @@
+import { ADD_POST, GET_ERRORS } from "./types";
+import axios from "axios";
+
+export const addPost = postData => dispatch => {
+  axios
+    .post("/api/posts")
+    .then(res => {
+      dispatch({
+        type: ADD_POST,
+        payload: res.data
+      });
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
