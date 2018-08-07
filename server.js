e026f07897b74f439f623fc314028aa3c0573427
tranki1 <kimhieutran100@gmail.com>
2018-08-08 01:05:09 +0300

    deploy

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
-const path = required("path");
+const path = require("path");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
@@ -10,37 +10,40 @@ const posts = require("./routes/api/posts");
 
 const app = express();
 
-//body pareser middleware
+// Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// DB config
+// DB Config
 const db = require("./config/keys").mongoURI;
 
-//connect to MongoDB
+// Connect to MongoDB
 mongoose
   .connect(db)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
-//passport middleware
+// Passport middleware
 app.use(passport.initialize());
 
-//passport config
+// Passport Config
 require("./config/passport")(passport);
-// Use route
+
+// Use Routes
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
-//Server static assets if in production
+
+// Server static assets if in production
 if (process.env.NODE_ENV === "production") {
-  //Set static folder
+  // Set static folder
   app.use(express.static("client/build"));
+
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
-//for heroku port and local port
 const port = process.env.PORT || 5000;
+
 app.listen(port, () => console.log(`Server running on port ${port}`));
